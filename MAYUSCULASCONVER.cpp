2265a35fc26301cfe202b5f6b695e8760d495bef
Miguel Matul <miguemat4@gmail.com>
2021-05-04 09:35:13 -0600

    Desafío

--- MAYUSCULASCONVER.cpp
+++ MAYUSCULASCONVER.cpp
@@ -3,28 +3,15 @@
 
 using namespace std;
 
-int main() {
-	
-	string texto ;
-	string resultado ;
-	cout << "Ingrese un texto: " << endl;
-
-	getline(cin, texto);
-
-	for (int i = 0; i < texto.size(); i++) {
-
-		char letra = texto[i];
-		if (letra >= 97 && letra <= 122) {
-			char mayuscula = letra - 32;
-			resultado = resultado + mayuscula;
-		}
-		else {
-			resultado = resultado + letra;
-		}
-	}
-	cout << "El resultado es: " << endl;
-	cout << resultado << endl;
-
-	
-	return 0;
+int main()
+{
+    /*
+     * Escriba un programa que remueva los elementos duplicados de cualquier arreglo, debe mostrar en pantalla el arreglo resultante
+     * Ejemplo 1: array = [1,2,2,3] array2 = [2,2] ---> Resultado: [1,2,3]
+     * Ejemplo 2: array = [0,0,0,1,2,2,3,3,3,3,3,3,4,4,4] ---> Resultado: [0,1,2,3,4]
+     * Consejo: Asuma que el arreglo que se probará siempre va a estar ordenado
+     */
+    cout << "Challenge #1" << endl;
+    int arreglo[] = {0, 1, 1, 1, 2, 2};  // Empiece con este arreglo ya existente, el docente modificará este arreglo para pruebas
+    return 0;
 }
